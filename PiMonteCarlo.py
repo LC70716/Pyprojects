297dfee2eb6fdb05ae7b9f279f05acbd3419a1f3
LC70716 <contileonardo59@gmail.com>
2023-10-29 13:17:31 +0100

    Formatting using BLACK

--- PiMonteCarlo.py
+++ PiMonteCarlo.py
@@ -1,24 +1,24 @@
-#%%
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 
 accepted = 0
 total = 10000000
 
-for i in range(1,total+1) :
+for i in range(1, total + 1):
     x = np.random.random()
     y = np.random.random()
     dist = np.sqrt(x**2 + y**2)
-    if dist <= 1 :
-       accepted+=1
- #       plt.xlim(0,1)
- #       plt.ylim(0,1)
- #       plt.scatter(x,y,color="green")
- #   else:
- #      plt.xlim(0,1)
- #      plt.ylim(0,1)
- #      plt.scatter(x,y,color="red")
-    pi = 4 * accepted/i
+    if dist <= 1:
+        accepted += 1
+    #       plt.xlim(0,1)
+    #       plt.ylim(0,1)
+    #       plt.scatter(x,y,color="green")
+    #   else:
+    #      plt.xlim(0,1)
+    #      plt.ylim(0,1)
+    #      plt.scatter(x,y,color="red")
+    pi = 4 * accepted / i
     err = np.pi - pi
-    print("N = %d; pi = %5.10f; err = %5.2e" % (i,pi,err))
+    print("N = %d; pi = %5.10f; err = %5.2e" % (i, pi, err))
 # %%
--- TwoDRandWalk.py
+++ TwoDRandWalk.py
@@ -1,78 +1,81 @@
-#%%
+# %%
 import numpy as np
 import matplotlib.pyplot as plt
 
-#random walk process,only modify p_l,p_u,steps,and graphics stuff
+# random walk process,only modify p_l,p_u,steps,and graphics stuff
 
 p_l = 0.5
-p_r = 1-p_l
+p_r = 1 - p_l
 p_u = 0.5
-p_d = 1-p_u
+p_d = 1 - p_u
 simulations = 10000
 
-x_hists = [[]] #for animation plotting purposes
+x_hists = [[]]  # for animation plotting purposes
 y_hists = [[]]
 
 Squaredistances = []
 delta_xs = []
 delta_ys = []
 
-def GetDelta(hist_array) :
-    return hist_array[len(hist_array)-1] - hist_array[0]
 
-def GetSquareDist( x_hist , y_hist) :
+def GetDelta(hist_array):
+    return hist_array[len(hist_array) - 1] - hist_array[0]
+
+
+def GetSquareDist(x_hist, y_hist):
     dx = GetDelta(x_hist)
     dy = GetDelta(y_hist)
     return dx**2 + dy**2
 
+
 steps = 1000
 
-for n in range(0,simulations):
- x = 0
- y = 0
- x_hist = [0] #for animation plotting purposes
- y_hist = [0] 
- for i in range(0,steps):
-    drawx = np.random.random()
-    drawy = np.random.random()
-    if drawx <= p_l :
-        x += 1
-        x_hist.append(x)
-    else:
-        x -= 1
-        x_hist.append(x)
-    if drawy <= p_l :
-        y += 1
-        y_hist.append(y)
-    else:
-        y -= 1
-        y_hist.append(y)
-    #plt.xlim(-50,50)
-    #plt.ylim(-50,50)
-    #plt.scatter(x,y,color="red",marker="x")
-    #plt.plot(x_hist, y_hist)
-    #plt.pause(0.3)
- x_hists.append(x_hist)
- y_hists.append(y_hist)
- delta_xs.append(GetDelta(x_hist))
- delta_ys.append(GetDelta(y_hist))
- Squaredistances.append(GetSquareDist(x_hist,y_hist))
+for n in range(0, simulations):
+    x = 0
+    y = 0
+    x_hist = [0]  # for animation plotting purposes
+    y_hist = [0]
+    for i in range(0, steps):
+        drawx = np.random.random()
+        drawy = np.random.random()
+        if drawx <= p_l:
+            x += 1
+            x_hist.append(x)
+        else:
+            x -= 1
+            x_hist.append(x)
+        if drawy <= p_l:
+            y += 1
+            y_hist.append(y)
+        else:
+            y -= 1
+            y_hist.append(y)
+        # plt.xlim(-50,50)
+        # plt.ylim(-50,50)
+        # plt.scatter(x,y,color="red",marker="x")
+        # plt.plot(x_hist, y_hist)
+        # plt.pause(0.3)
+    x_hists.append(x_hist)
+    y_hists.append(y_hist)
+    delta_xs.append(GetDelta(x_hist))
+    delta_ys.append(GetDelta(y_hist))
+    Squaredistances.append(GetSquareDist(x_hist, y_hist))
 
 plt.figure(0)
-plt.xlim(-50,50)
-plt.ylim(-50,50)
-#plt.scatter(x,y,color="red",marker="x")
-for j in range(0,len(x_hists)) :
- plt.plot(x_hists[j], y_hists[j])
+plt.xlim(-50, 50)
+plt.ylim(-50, 50)
+# plt.scatter(x,y,color="red",marker="x")
+for j in range(0, len(x_hists)):
+    plt.plot(x_hists[j], y_hists[j])
 
 plt.figure(1)
 plt.hist(delta_xs)
-print("mean dx = %5.2f" % (np.mean(x_hist)) )
+print("mean dx = %5.2f" % (np.mean(x_hist)))
 plt.figure(2)
 plt.hist(delta_ys)
-print("mean dy = %5.2f" % (np.mean(y_hist)) )
+print("mean dy = %5.2f" % (np.mean(y_hist)))
 plt.figure(3)
 plt.hist(Squaredistances)
-print("mean SD = %5.2f" % (np.mean(Squaredistances)) )
-    
+print("mean SD = %5.2f" % (np.mean(Squaredistances)))
+
 # %%
